afe310608960c11622c0011df808a32d50a33b26
丧心病狂大薯条 <wldyxhyy@sina.com>
2017-04-04 20:02:16 +0800

    change tmpcall

--- sample/tool_test/main.cpp
+++ sample/tool_test/main.cpp
@@ -1,8 +1,39 @@
-#include "../../tool/tmpcall.h"
 
+#include "../../tool/tmpcall.h"
+#include <iostream>
+using namespace std;
+using namespace PO::TmpCall;
+template<size_t i> using si = std::integral_constant<size_t, i>;
+template<char i> using ci = std::integral_constant<char, i>;
+
+template<typename ...T> struct op {};
+
+template<typename T, typename K> struct lable_size
+{
+	using type = std::integral_constant<size_t, sizeof(T)>;
+};
+
+class A { A() {} };
+class B {};
+class C {};
+
+template<typename T> struct is_A
+{
+	using type = std::is_same<T, A>;
+};
+
+struct OPC
+{
+	template<typename ...T>
+	struct in
+	{
+		using type = int;
+	};
+};
 
 
 int main()
 {
-
+	cout << typeid(call < append < A, B, C, A, B> , localizer<make_func_t<is_A>>, self > ).name() << endl;
+	system("pause");
 }
